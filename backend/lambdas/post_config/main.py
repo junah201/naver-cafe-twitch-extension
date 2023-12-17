3dcef0f16e27140aa7bf7ee6d158db05f27add55
junah201~ <junah.dev@gmail.com>
2023-12-17 22:30:34 +0900

    ✨post config lambda 추가

---
+++ backend/lambdas/post_config/main.py
@@ -0,0 +1,63 @@
+import json
+
+import boto3
+
+
+def lambda_handler(event, context):
+    id = event.get(["pathParameters"], {}).get("id", None)
+
+    if not id:
+        return {
+            "statusCode": "400",
+            "headers": {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            "body": json.dumps({
+                "message": "'id' is required"
+            })
+        }
+
+    body = json.loads(event.get("body", "{}"))
+
+    keys = ["panel_title", "cafe_name", "cafe_id", "cafe_menu_id", "cafe_board_type"]
+
+    for key in keys:
+        if key not in body:
+            return {
+                "statusCode": "400",
+                "headers": {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                "body": json.dumps({
+                    "message": f"'{key}' is required"
+                })
+            }
+
+    dynamodb = boto3.resource(
+        "dynamodb",
+        region_name="ap-northeast-2"
+    )
+
+    table = dynamodb.Table("Channel")
+
+    response = table.put_item(
+        Item={
+            "id": id,
+            "panel_title": body["panel_title"],
+            "cafe_name": body["cafe_name"],
+            "cafe_id": body["cafe_id"],
+            "cafe_menu_id": body["cafe_menu_id"],
+            "cafe_board_type": body["cafe_board_type"]
+        }
+    )
+
+    return {
+        "statusCode": "200",
+        "headers": {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        "body": json.dumps(response)
+    }
