bd4112d1c982626c9fc22cb6920df4f71982d28c
junah201~ <junah.dev@gmail.com>
2023-12-17 22:05:34 +0900

    👷terraform role module 추가

---
+++ backend/terraform/modules/role/variable.tf
@@ -0,0 +1,17 @@
+variable "name" {
+  type        = string
+  description = "Name of the role"
+}
+
+variable "policy" {
+  type        = string
+  description = "Policy of the role"
+}
+
+variable "role_sid" {
+  type        = string
+  description = "Role sid of the role"
+  default     = ""
+}
+
+
