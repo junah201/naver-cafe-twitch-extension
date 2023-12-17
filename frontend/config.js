0eb74f70ff76abdbf495316e12ba909d233bb38e
junah201 <junah.dev@gmail.com>
2023-12-17 23:38:43 +0900

    ✨API 주소 변경

--- frontend/config.js
+++ frontend/config.js
@@ -1,129 +1,129 @@
-const url = `https://port-0-naver-cafe-twitch-extension-otjl2cli1ot5ff.sel4.cloudtype.app`;
+const url = `https://navercafe.junah.dev`;
 
 window.Twitch.ext.onAuthorized(function (params) {
-	const channelId = params.channelId;
-	function saveConfig(channelId, title, keyword) {
-		fetch(
-			`${url}/config?channel_id=${channelId}&title=${title}&keyword=${keyword}`,
-			{
-				method: "POST",
-			}
-		).then((response) => console.log(response));
+  const channelId = params.channelId;
+  function saveConfig(channelId, title, keyword) {
+    fetch(
+      `${url}/config?channel_id=${channelId}&title=${title}&keyword=${keyword}`,
+      {
+        method: "POST",
+      }
+    ).then((response) => console.log(response));
 
-		console.log("Saved");
-	}
+    console.log("Saved");
+  }
 
-	let boards = {};
+  let boards = {};
 
-	boardsFetchButton = document.getElementById("fetchButton");
-	saveButton = document.getElementById("selectButton");
-	console.log(boardsFetchButton);
-	console.log(saveButton);
+  boardsFetchButton = document.getElementById("fetchButton");
+  saveButton = document.getElementById("selectButton");
+  console.log(boardsFetchButton);
+  console.log(saveButton);
 
-	boardsFetchButton.addEventListener("click", function () {
-		const cafeNameInput = document.getElementById("cafeNameInput");
+  boardsFetchButton.addEventListener("click", function () {
+    const cafeNameInput = document.getElementById("cafeNameInput");
 
-		if (cafeNameInput.value == "") {
-			alert("카페 이름을 입력해주세요.");
-			return;
-		}
+    if (cafeNameInput.value == "") {
+      alert("카페 이름을 입력해주세요.");
+      return;
+    }
 
-		fetch(`${url}/${cafeNameInput.value}/boards`, {
-			method: "GET",
-			headers: {
-				accept: "application/json",
-			},
-		})
-			.then((res) => {
-				console.log(res);
-				return res.json();
-			})
-			.then((data) => {
-				boardsSelect = document.getElementById("boardSelect");
-				boardsSelect.innerHTML = "";
-				data.forEach((board) => {
-					boards[board.cafe_menu_id] = board;
-					boardsSelect.innerHTML += `<option value="${board.cafe_menu_id}">${board.board_name}</option>`;
-				});
-			});
-	});
+    fetch(`${url}/${cafeNameInput.value}/boards`, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        boardsSelect = document.getElementById("boardSelect");
+        boardsSelect.innerHTML = "";
+        data.forEach((board) => {
+          boards[board.cafe_menu_id] = board;
+          boardsSelect.innerHTML += `<option value="${board.cafe_menu_id}">${board.board_name}</option>`;
+        });
+      });
+  });
 
-	saveButton.addEventListener("click", function () {
-		const titleInput = document.getElementById("titleInput");
-		const cafeNameInput = document.getElementById("cafeNameInput");
-		const boardSelect = document.getElementById("boardSelect");
-		const alertSpan = document.getElementById("alert");
+  saveButton.addEventListener("click", function () {
+    const titleInput = document.getElementById("titleInput");
+    const cafeNameInput = document.getElementById("cafeNameInput");
+    const boardSelect = document.getElementById("boardSelect");
+    const alertSpan = document.getElementById("alert");
 
-		alertSpan.innerHTML = `저장 중...`;
+    alertSpan.innerHTML = `저장 중...`;
 
-		fetch(`${url}/${channelId}/config`, {
-			method: "POST",
-			headers: {
-				accept: "application/json",
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				panel_title: titleInput.value,
-				cafe_name: cafeNameInput.value,
-				cafe_id: boards[boardSelect.value].cafe_id,
-				cafe_menu_id: boardSelect.value,
-				cafe_board_type: boards[boardSelect.value].cafe_board_type,
-			}),
-		})
-			.then((res) => {
-				if (res.status === 200) {
-					alertSpan.innerHTML = `저장되었습니다.`;
-					return;
-				}
-				alertSpan.innerHTML = `저장에 실패했습니다. 알 수 없는 오류`;
-			})
-			.catch((err) => {
-				alert(`저장에 실패했습니다.\n\n${err}`);
-			});
-	});
+    fetch(`${url}/${channelId}/config`, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        panel_title: titleInput.value,
+        cafe_name: cafeNameInput.value,
+        cafe_id: boards[boardSelect.value].cafe_id,
+        cafe_menu_id: boardSelect.value,
+        cafe_board_type: boards[boardSelect.value].cafe_board_type,
+      }),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          alertSpan.innerHTML = `저장되었습니다.`;
+          return;
+        }
+        alertSpan.innerHTML = `저장에 실패했습니다. 알 수 없는 오류`;
+      })
+      .catch((err) => {
+        alert(`저장에 실패했습니다.\n\n${err}`);
+      });
+  });
 
-	fetch(`${url}/${channelId}/config`, {
-		method: "GET",
-		headers: {
-			accept: "application/json",
-		},
-	})
-		.then((res) => {
-			return res.json();
-		})
-		.then((configData) => {
-			console.log(configData);
-			titleInput = document.getElementById("titleInput");
-			titleInput.value = configData?.panel_title;
-			cafeNameInput = document.getElementById("cafeNameInput");
-			cafeNameInput.value = configData?.cafe_name;
+  fetch(`${url}/${channelId}/config`, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+    },
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((configData) => {
+      console.log(configData);
+      titleInput = document.getElementById("titleInput");
+      titleInput.value = configData?.panel_title;
+      cafeNameInput = document.getElementById("cafeNameInput");
+      cafeNameInput.value = configData?.cafe_name;
 
-			if (configData?.cafe_name) {
-				fetch(`${url}/${configData?.cafe_name}/boards`, {
-					method: "GET",
-					headers: {
-						accept: "application/json",
-					},
-				})
-					.then((res) => {
-						console.log(res);
-						return res.json();
-					})
-					.then((data) => {
-						boardsSelect = document.getElementById("boardSelect");
-						boardsSelect.innerHTML = "";
-						data.forEach((board) => {
-							boards[board.cafe_menu_id] = board;
-							boardsSelect.innerHTML += `<option value="${board.cafe_menu_id}">${board.board_name}</option>`;
-						});
+      if (configData?.cafe_name) {
+        fetch(`${url}/${configData?.cafe_name}/boards`, {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+          },
+        })
+          .then((res) => {
+            console.log(res);
+            return res.json();
+          })
+          .then((data) => {
+            boardsSelect = document.getElementById("boardSelect");
+            boardsSelect.innerHTML = "";
+            data.forEach((board) => {
+              boards[board.cafe_menu_id] = board;
+              boardsSelect.innerHTML += `<option value="${board.cafe_menu_id}">${board.board_name}</option>`;
+            });
 
-						boardsSelect.value = configData?.cafe_menu_id;
-					})
-					.catch((err) => {
-						console.log(err);
-						alertSpan = document.getElementById("alert");
-						alertSpan.innerHTML = `카페 아이디가 잘못되었습니다. ${err}`;
-					});
-			}
-		});
+            boardsSelect.value = configData?.cafe_menu_id;
+          })
+          .catch((err) => {
+            console.log(err);
+            alertSpan = document.getElementById("alert");
+            alertSpan.innerHTML = `카페 아이디가 잘못되었습니다. ${err}`;
+          });
+      }
+    });
 });
--- frontend/panel.js
+++ frontend/panel.js
@@ -1,59 +1,59 @@
-const url = `https://port-0-naver-cafe-twitch-extension-otjl2cli1ot5ff.sel4.cloudtype.app`;
+const url = `https://navercafe.junah.dev`;
 
 window.Twitch.ext.onAuthorized(function (params) {
-	const channelId = params.channelId;
-	console.log(channelId);
+  const channelId = params.channelId;
+  console.log(channelId);
 
-	function reloadItem() {
-		fetch(`${url}/${channelId}/posts`, {
-			method: "GET",
-		})
-			.then((res) => {
-				return res.json();
-			})
-			.then((data) => {
-				for (const idx in data) {
-					const item = document.getElementById(idx);
-					if (item == null) {
-						const table = document.getElementById("table");
-						console.log(table);
-						if (idx == 0) {
-							table.innerHTML += `<div class = "item" id = ${idx}><a href = ${data[idx].link} target="_blank">${data[idx].title}</a></div>`;
-						} else {
-							table.innerHTML += `<br/><div class = "item" id = ${idx}><a href = ${data[idx].link} target="_blank">${data[idx].title}</a></div>`;
-						}
-					} else {
-						item.innerHTML = `<a href = ${data[idx].link} target="_blank">${data[idx].title}</a>`;
-					}
-					console.log(data[idx]);
-				}
-			});
-	}
+  function reloadItem() {
+    fetch(`${url}/${channelId}/posts`, {
+      method: "GET",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        for (const idx in data) {
+          const item = document.getElementById(idx);
+          if (item == null) {
+            const table = document.getElementById("table");
+            console.log(table);
+            if (idx == 0) {
+              table.innerHTML += `<div class = "item" id = ${idx}><a href = ${data[idx].link} target="_blank">${data[idx].title}</a></div>`;
+            } else {
+              table.innerHTML += `<br/><div class = "item" id = ${idx}><a href = ${data[idx].link} target="_blank">${data[idx].title}</a></div>`;
+            }
+          } else {
+            item.innerHTML = `<a href = ${data[idx].link} target="_blank">${data[idx].title}</a>`;
+          }
+          console.log(data[idx]);
+        }
+      });
+  }
 
-	function reloadTitle() {
-		fetch(`${url}/${channelId}/config`, {
-			method: "GET",
-		})
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				titleH1 = document.getElementById("title");
-				titleH1.innerHTML = data.panel_title;
-				titleA = document.querySelector("#title > a");
-				titleA.href = `https://cafe.naver.com/${data.cafe_name}`;
-			});
-	}
+  function reloadTitle() {
+    fetch(`${url}/${channelId}/config`, {
+      method: "GET",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        titleH1 = document.getElementById("title");
+        titleH1.innerHTML = data.panel_title;
+        titleA = document.querySelector("#title > a");
+        titleA.href = `https://cafe.naver.com/${data.cafe_name}`;
+      });
+  }
 
-	reloadTitle();
-	reloadItem();
+  reloadTitle();
+  reloadItem();
 
-	refreshButton = document.getElementById("refresh");
-	refreshButton.addEventListener("mousedown", function () {
-		reloadItem();
-		refreshButton.className = "display";
-	});
-	refreshButton.addEventListener("mouseup", function () {
-		refreshButton.className = "undisplay";
-	});
+  refreshButton = document.getElementById("refresh");
+  refreshButton.addEventListener("mousedown", function () {
+    reloadItem();
+    refreshButton.className = "display";
+  });
+  refreshButton.addEventListener("mouseup", function () {
+    refreshButton.className = "undisplay";
+  });
 });
